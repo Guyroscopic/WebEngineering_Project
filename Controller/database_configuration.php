5f6be8b362707efac4192c5d68abaeac709e1ff7
LaraibHere <laraibarjamand@gmail.com>
2021-01-03 17:49:25 +0500

    Database Config File Created

--- Controller/database_configuration.php
+++ Controller/database_configuration.php
@@ -9,7 +9,7 @@
 	define('DB_HOST', 'localhost');
 	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', '');
-	define('DB_NAME', 'users');
+	define('DB_NAME', 'webproject');
 
 	/* Connecting to MySql Database*/
 	$databse_connection = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
