889335b045fa38dfdb631847d571813039cdaea1
Ghazala Bibi <ghazala01@users.noreply.github.com>
2021-01-12 20:37:08 +0500

    HTML and CSS files of home,about,createtutorial,viewtutorial,login,registration,studentprofile,contact,tutors and teacherprofile are added

---
+++ web project/assets/js/main.js
@@ -0,0 +1,86 @@
+$(function() {
+    
+    "use strict";
+
+    
+    // navbar on the top all the time
+    
+    $(window).on('scroll', function(event) {    
+        var scroll = $(window).scrollTop();
+        if (scroll < 10) {
+            $(".navbar-area").removeClass("sticky");
+        } else{
+            $(".navbar-area").addClass("sticky");
+        }
+    }); 
+
+    
+    //Side bar
+
+    $('[href="#side-menu-right"], .overlay-right').on('click', function (event) {
+        $('.sidebar-right, .overlay-right').addClass('open');
+    });
+
+    $('[href="#close"], .overlay-right').on('click', function (event) {
+        $('.sidebar-right, .overlay-right').removeClass('open');
+    });
+    
+    
+    
+    //Navigation bar on top
+
+    $('.container').imagesLoaded(function () {
+        var $grid = $('.grid').isotope({
+            // options
+            transitionDuration: '1s'
+        }); 
+        $('.portfolio-menu ul li').on('click', function (event) {
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            event.preventDefault();
+        });
+    });      
+
+    // provides movement of tutors blocks one after the other
+
+    $('.testimonial-active').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 800,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+    new WOW().init();
+     
+});
