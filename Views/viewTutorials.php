a6656322a3596d301e46572a2ee18a9ffa60fa08
Ghazala Bibi <gbibi.bese18seecs@seecs.edu.pk>
2021-01-17 01:42:29 +0500

    Modified Create Tutorial and View Tutorial Page

--- Views/viewTutorials.php
+++ Views/viewTutorials.php
@@ -56,6 +56,8 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Style css -->
+    <link rel="stylesheet" href="../assets/css/style.css">
   <style>
     @import url("https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800");
     body {
@@ -188,6 +190,46 @@
   ?>
 
 </div>
+
+   <!--===== FOOTERpart starts ======-->
+    
+  <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About our Project</h6>
+            <p class="text-justify">Our project is tutoring website for students where teachers can post and edit tutorials while students can see tutorials and can examine themselves through quizes. It is managed by Admin. We have used PHP for backend and HTML, CSS, jQuery and Bootstrap for frontend.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Contact Information</h6>
+            <ul>
+              <p>SEECS</p>
+              <p>NUST, H12 Campus</p>
+              <p>ISLAMABAD</p>
+            </ul>
+          </div>
+          <br><br><br>
+          <div class="col-xs-6 col-md-3"><br>
+            <ul>
+              <p>BESE9B</p>
+              <p>Batch'2k18</p>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2021 All rights reserved by Babloo Gang
+            </p>
+          </div>
+        </div>
+      </div>
+</footer>
+
+    <!--===== FOOTERpart ends ======-->
 
 </body>
 </html> 
